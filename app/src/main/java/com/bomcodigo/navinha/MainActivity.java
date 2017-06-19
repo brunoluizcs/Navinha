28e4e077ddf2d61539d4cf89d324bf25d00c1123
bruno <bruno.luiz88@gmail.com>
2017-06-19 20:13:53 -0300

    Inclusão do Google Play Game Service

--- app/src/main/java/com/bomcodigo/navinha/MainActivity.java
+++ app/src/main/java/com/bomcodigo/navinha/MainActivity.java
@@ -4,24 +4,40 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.hardware.SensorManager;
-import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
 import com.bomcodigo.navinha.game.DeviceSettings;
 import com.bomcodigo.navinha.game.scenes.TitleScreen;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.games.Games;
+import com.google.example.games.basegameutils.BaseGameUtils;
 
 import org.cocos2d.layers.CCScene;
 import org.cocos2d.nodes.CCDirector;
 import org.cocos2d.opengl.CCGLSurfaceView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener{
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private GoogleApiClient mGoogleApiClient;
+    private static int RC_SIGN_IN = 9001;
+    private boolean mResolvingConnectionFailure = false;
+    private boolean mAutoStartSignInflow = true;
+    private boolean mSignInClicked = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -34,13 +50,97 @@ public class MainActivity extends AppCompatActivity {
 
         configSensorManager();
 
+        //TODO: Conectar a API
+        this.mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .build();
+
+
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()){
+            Log.d(TAG,"Player Connected");
+        }else{
+            Log.d(TAG,"Need Connection");
+        }
+
         CCScene scene = new TitleScreen().scene();
         CCDirector.sharedDirector().runWithScene(scene);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //TODO: Remover esta linha
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //TODO: Remover esta linha
+        mGoogleApiClient.disconnect();
+    }
+
+
 
     public void configSensorManager(){
         SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         DeviceSettings.setSensorManager(sensorManager);
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG,"Player Connected");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG,"Attempt Reconnect");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        if (mResolvingConnectionFailure){
+            Log.d(TAG,"Connection failure already resolving");
+            return;
+        }
+
+        if (mSignInClicked || mAutoStartSignInflow){
+            mAutoStartSignInflow= false;
+            mSignInClicked =false;
+            mResolvingConnectionFailure= true;
+            if (!BaseGameUtils.resolveConnectionFailure(this,
+                    mGoogleApiClient,connectionResult,
+                    RC_SIGN_IN,R.string.sign_in_other_error)){
+                mResolvingConnectionFailure = false;
+            }
+        }
+        //TODO: Exibir botão de login
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if  (requestCode == RC_SIGN_IN){
+            mSignInClicked = false;
+            mResolvingConnectionFailure = false;
+            if (resultCode == RESULT_OK){
+                mGoogleApiClient.connect();
+            }else{
+                BaseGameUtils.showActivityResultError(this,
+                        requestCode, resultCode, R.string.sign_in_failed);
+            }
+        }
+    }
+
+    private void signInClicked(){
+        mSignInClicked = true;
+        mGoogleApiClient.connect();
+    }
+
+    private void signOutClicked(){
+        mSignInClicked = false;
+        Games.signOut(mGoogleApiClient);
+    }
 }
